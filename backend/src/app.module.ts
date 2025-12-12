12e868f1bd03034ddb5beab4f62e3bb4eff9cd6e
Renzo Jorge <renzoch99@gmail.com>
2025-12-12 15:58:59 -0500

    feat(backend): Agrega rate limited para evitar spam de peticiones

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,27 @@ import { ModulesModule } from './modules/modules.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { typeOrmConfig } from './config/orm.config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(typeOrmConfig()),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // Tiempo en milisegundos (60 segundos)
+        limit: 10, // Número máximo de peticiones permitidas en ese tiempo
+      },
+    ]),
     ModulesModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
