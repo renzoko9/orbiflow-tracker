b4f75629a97d5b85a098130b96f08c9f1ad805ee
Renzo Jorge <renzoch99@gmail.com>
2026-04-07 00:29:51 -0500

    fix(backend): Aumenta rate limit de ThrottlerModule a 20 req/min

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -13,7 +13,7 @@ import { APP_GUARD } from '@nestjs/core';
     ThrottlerModule.forRoot([
       {
         ttl: 60000, // Tiempo en milisegundos (60 segundos)
-        limit: 10, // Número máximo de peticiones permitidas en ese tiempo
+        limit: 20, // Número máximo de peticiones permitidas en ese tiempo
       },
     ]),
     ModulesModule,
